f62e26ee1fc703c83cc4eeaf0f64f0ab9f85196e
td <tom.depta@gmail.com>
2017-01-13 14:57:16 +0100

    First draft of displaying board on game activity

--- app/src/main/java/com/pum/voicememory/view/GameActivity.java
+++ app/src/main/java/com/pum/voicememory/view/GameActivity.java
@@ -1,7 +1,11 @@
 package com.pum.voicememory.view;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.pum.voicememory.R;
@@ -19,6 +23,32 @@ public class GameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+        LinearLayout layout = (LinearLayout)findViewById(R.id.board_placeholder);
+        for (int i = 0; i < 4; i++) {
+            LinearLayout row = new LinearLayout(this);
+            row.setOrientation(LinearLayout.HORIZONTAL);
+            for (int j = 0; j < 3; j++) {
+                TextView tv = new TextView(this);
+                tv.setWidth(40);
+                tv.setHeight(40);
+                tv.setBackgroundColor(Color.CYAN);
+
+                LinearLayout.LayoutParams llp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                llp.setMargins(20, 0, 0, 0); // (left, top, right, bottom)
+                tv.setLayoutParams(llp);
+
+                row.addView(tv);
+            }
+            LinearLayout.LayoutParams llp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            llp.setMargins(0, 0, 0, 10); // (left, top, right, bottom)
+            layout.addView(row, llp);
+        }
+
+
+        initializeStopwatch();
+    }
+
+    private void initializeStopwatch() {
         timer = new Timer();
         TimerTask timerTask = new TimerTask() {
             @Override
